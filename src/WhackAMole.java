9e9dec4d9e1abc67e7d2f0ec888d77bcaee199c6
tlcrack <league@WTS14.attlocal.net>
2018-08-28 19:58:08 -0700

    WhackAMole ip

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,30 +1,49 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class WhackAMole implements ActionListener{
-	JFrame frame= new JFrame();
-	JPanel panel= new JPanel();
-	
-	void start() {
-		
-		for (int i = 0; i < 22; i++) {
-			JButton button= new JButton();
+public class WhackAMole implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+
+	public static void main(String[] args) {
+		WhackAMole WAM = new WhackAMole();
+		WAM.start();
+	}
+
+	void drawButtons(int m) {
+
+		for (int i = 0; i < 24; i++) {
+			JButton button = new JButton();
 			panel.add(button);
 			button.addActionListener(this);
+
+			if (i == m) {
+				button.setText("Mole");
+			}
 		}
-		}
-	
-	
+
+	}
+
+	void start() {
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.setSize(300, 300);
+		Random r = new Random(24);
+
+		drawButtons(r);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		e.getSource();
+		System.out.println("Clicked!");
 	}
-
-	
 
 }
